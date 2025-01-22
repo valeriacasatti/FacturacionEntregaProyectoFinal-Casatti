04b8662524705e4f82d5b5fa864acda3206c5802
Valeria <valeria.casatti@gmail.com>
2025-01-22 16:32:28 -0300

    models

---
+++ facturacion/src/main/java/com/coderhouse/models/Producto.java
@@ -0,0 +1,49 @@
+package com.coderhouse.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"ventas"})
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String nombre;
+	
+	@Column(nullable=false)
+	private int precio;
+	
+	@Column(nullable=false)
+	private Integer stock;
+	
+	@ManyToMany(mappedBy = "productos", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Venta> ventas = new ArrayList<>();
+}
---
+++ facturacion/src/main/java/com/coderhouse/models/Venta.java
@@ -0,0 +1,58 @@
+package com.coderhouse.models;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"cliente", "productos"})
+
+@Entity
+@Table(name = "ventas")
+public class Venta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Date fecha;
+	
+	@Column(nullable = false)
+	private Integer total;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id", nullable = false)
+	private Cliente cliente;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+			name = "venta_producto",
+			joinColumns = @JoinColumn(name = "venta_id"),
+			inverseJoinColumns = @JoinColumn(name = "producto_id")
+			)
+	@JsonIgnore
+	private List<Producto> productos = new ArrayList<>();
+}
